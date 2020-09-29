039c0378e00e3033843310de6975e1d11a420988
Andres <andrespujalte@gmail.com>
2020-09-29 18:36:53 -0300

    Casi Terminado 29-09-2020

--- app/src/main/java/com/example/MyBook/Fragments/LoginRecoveryFragment.kt
+++ app/src/main/java/com/example/MyBook/Fragments/LoginRecoveryFragment.kt
@@ -1,60 +1,68 @@
 package com.example.MyBook.Fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
 import com.example.MyBook.R
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginRecoveryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginRecoveryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    lateinit var bt_login_recover_ok : Button
+    lateinit var bt_login_recover_cancel : Button
+    lateinit var et_login_recovery_email: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_recovery, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginRecoveryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginRecoveryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        var v : View = inflater.inflate(R.layout.fragment_login_recovery, container, false)
+        var password : String = LoginRecoveryFragmentArgs.fromBundle(requireArguments()).password.toString()
+        var email : String = LoginRecoveryFragmentArgs.fromBundle(requireArguments()).email.toString()
+        et_login_recovery_email = v.findViewById(R.id.et_login_recovery_email)
+        bt_login_recover_ok = v.findViewById(R.id.bt_login_recover_ok)
+        bt_login_recover_cancel = v.findViewById(R.id.bt_login_recover_cancel)
+        bt_login_recover_ok.setOnClickListener(){
+            if(email == et_login_recovery_email.text.toString()){
+                sendEmail(
+                    email,
+                    "Recuperacion de contraseña de MyBook App",
+                    "Su contraseña actual es: " + password
+                )
+                Snackbar.make(v, "EMAIL ENVIADO", Snackbar.LENGTH_SHORT)
+            }else
+            {
+                Snackbar.make(v, "EMAIL INCORRECTO", Snackbar.LENGTH_SHORT)
             }
+
+        }
+        bt_login_recover_cancel.setOnClickListener(){
+
+        }
+        return v
+    }
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+
+        val mIntent = Intent(Intent.ACTION_SEND)
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+        try {
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){ }
     }
 }
