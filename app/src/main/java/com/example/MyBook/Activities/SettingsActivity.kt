6e59b183d7aadd54d8e362cd0650b19b6259027a
Andres <andrespujalte@gmail.com>
2020-09-29 13:22:44 -0300

    Casi Terminado

--- app/src/main/java/com/example/MyBook/Activities/SettingsActivity.kt
+++ app/src/main/java/com/example/MyBook/Activities/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.MyBook.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.preference.PreferenceFragmentCompat
 import com.example.MyBook.R
 
@@ -15,11 +16,23 @@ class SettingsActivity : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                super.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
         }
     }
 }
